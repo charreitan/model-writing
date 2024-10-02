98fdfc3df808c72687c0432b8c4b7730fe4b2a80
CHARLOTTE <creitan@uvm.edu>
2024-10-02 10:22:04 -0400

    Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// apartment building //
+    // properties //
+        // number of apartments //
+        // building name //
+        // location //
+        // building material //
+        // cheap or expensive // 
+        // amenities //
+            // gym, pool, rooftop, laundry, elevator //
+    
+    // events //
+        // ride elevator //
+        // walk upstairs //
+        // open door //
+        // unlock or scan door //   
+    
+    // methods //
+        // 
